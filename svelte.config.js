c77a8e459c8d6171f53d343f5e387c8f3f1204b4
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2023-06-24 23:45:16 +0200

    multi adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,9 @@
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static'; // This was changed from adapter-auto
-
+import adapterAuto from '@sveltejs/adapter-auto';
+import adapterStatic from '@sveltejs/adapter-static'; // This was changed from adapter-auto
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,7 +15,8 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({ strict: false }) // base on the environment, this will be adapter-static or adapter-vercel
+		adapter: process.env.IS_DESKTOP ? adapterStatic({ strict: false }) : adapterAuto()
+		// TODO: maybe can set IS_DESKTOP to true when running tauri???
 	}
 };
 
